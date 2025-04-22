28813d745ab4459776fa0772f294cfda575ef76d
Bandi Matta Reddy <bandimattareddy@gmail.com>
2025-04-22 11:52:41 +0530

    Updates

---
+++ precache-manifest.0bf430bdae9e4832d2952b6706170eda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9f76424f1118e5769fb93577930df3",
+    "url": "/ml-portfolio/index.html"
+  },
+  {
+    "revision": "1fc54d4b064f66a0fd2d",
+    "url": "/ml-portfolio/static/css/main.77e67bd8.chunk.css"
+  },
+  {
+    "revision": "177ce48fe8e2d2b8fcbc",
+    "url": "/ml-portfolio/static/js/2.938771d9.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/ml-portfolio/static/js/2.938771d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68280009ff1c724c8e5",
+    "url": "/ml-portfolio/static/js/3.82d32a71.chunk.js"
+  },
+  {
+    "revision": "1fc54d4b064f66a0fd2d",
+    "url": "/ml-portfolio/static/js/main.f0ebcfcc.chunk.js"
+  },
+  {
+    "revision": "0f4443d41dedd982f8c6",
+    "url": "/ml-portfolio/static/js/runtime-main.5cd9091c.js"
+  }
+]);
